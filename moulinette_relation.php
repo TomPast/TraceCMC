ce57990734cfac8467a636ed2087de7c4db85de2
Jehanno <manuel.jehanno.etu@univ-lemans.fr>
2020-12-18 23:49:31 +0100

    oubli

--- moulinette_relation.php
+++ moulinette_relation.php
@@ -3,7 +3,7 @@
     *  Moulinette PHP qui va ressortir les informations nécéssaires uniquement et les stocker dans un fichier JSON
     */
 
-    $mysqli= new mysqli("localhost", "root", "", "trace_forum");
+    $mysqli= new mysqli("localhost", "root", "", "traceforum");
 
     $JSONResult = array();
 
@@ -15,8 +15,10 @@
         $MessagePeriode = array();
         $queryMessagePeriode1 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Poster un nouveau message' OR `Titre`= 'Repondre a un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-02-01' AND '2009-02-15'";
         $MessagePeriode[0] = $mysqli->query($queryMessagePeriode1)-> fetch_row()[0];
-    }
 
+        echo $MessagePeriode[0];
+        echo "<br/>";
+    }
 
 
 ?>
