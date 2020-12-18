29d9b1eb37943710061f60350c9045efc54cbc2b
Jehanno <manuel.jehanno.etu@univ-lemans.fr>
2020-12-18 21:38:43 +0100

    Auto stash before merge of "Manu" and "origin/Manu"

---
+++ moulinette.php
@@ -0,0 +1,137 @@
+<?php
+    /*
+    *  Moulinette PHP qui va ressortir les informations nécéssaires uniquement et les stocker dans un fichier JSON
+    */
+
+    $mysqli= new mysqli("localhost", "root", "", "trace_forum");
+    
+    $JSONResult = array();
+
+    $queryUsers = "SELECT DISTINCT Utilisateur From transition";
+    $resultUsers = $mysqli->query($queryUsers);
+    while($r = $resultUsers -> fetch_row()){
+        $nom = $r[0];
+        //Contient le nombre de message écrit (poste ou réponse) par période de 15 jours
+        $MessagePeriode = array();
+        $queryMessagePeriode1 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Poster un nouveau message' OR `Titre`= 'Repondre a un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-02-01' AND '2009-02-15'";
+        $MessagePeriode[0] = $mysqli->query($queryMessagePeriode1)-> fetch_row()[0];
+
+        $queryMessagePeriode2 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Poster un nouveau message' OR `Titre`= 'Repondre a un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-02-15' AND '2009-02-29'";
+        $MessagePeriode[1] = $mysqli->query($queryMessagePeriode2)-> fetch_row()[0];
+
+        $queryMessagePeriode3 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Poster un nouveau message' OR `Titre`= 'Repondre a un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-03-01' AND '2009-03-15'";
+        $MessagePeriode[2] = $mysqli->query($queryMessagePeriode3)-> fetch_row()[0];
+
+        $queryMessagePeriode4 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Poster un nouveau message' OR `Titre`= 'Repondre a un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-03-15' AND '2009-03-31'";
+        $MessagePeriode[3] = $mysqli->query($queryMessagePeriode4)-> fetch_row()[0];
+
+        $queryMessagePeriode5 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Poster un nouveau message' OR `Titre`= 'Repondre a un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-04-01' AND '2009-04-15'";
+        $MessagePeriode[4] = $mysqli->query($queryMessagePeriode5)-> fetch_row()[0];
+
+        $queryMessagePeriode6 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Poster un nouveau message' OR `Titre`= 'Repondre a un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-04-15' AND '2009-04-31'";
+        $MessagePeriode[5] = $mysqli->query($queryMessagePeriode6)-> fetch_row()[0];
+
+        $queryMessagePeriode7 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Poster un nouveau message' OR `Titre`= 'Repondre a un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-05-01' AND '2009-05-15'";
+        $MessagePeriode[6] = $mysqli->query($queryMessagePeriode7)-> fetch_row()[0];
+
+        $queryMessagePeriode8 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Poster un nouveau message' OR `Titre`= 'Repondre a un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-05-15' AND '2009-05-31'";
+        $MessagePeriode[7] = $mysqli->query($queryMessagePeriode8)-> fetch_row()[0];
+        
+        //Contient le nombre de message affiché par période de 15 jours
+        $LecturePeriode = array();
+        $queryLecturePeriode1 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Afficher le contenu d\'un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-02-01' AND '2009-02-15'";
+        $LecturePeriode[0] = $mysqli->query($queryLecturePeriode1)-> fetch_row()[0];
+    
+        $queryLecturePeriode2 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Afficher le contenu d\'un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-02-15' AND '2009-02-29'";
+        $LecturePeriode[1] = $mysqli->query($queryLecturePeriode2)-> fetch_row()[0];
+    
+        $queryLecturePeriode3 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Afficher le contenu d\'un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-03-01' AND '2009-03-15'";
+        $LecturePeriode[2] = $mysqli->query($queryLecturePeriode3)-> fetch_row()[0];
+    
+        $queryLecturePeriode4 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Afficher le contenu d\'un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-03-15' AND '2009-03-31'";
+        $LecturePeriode[3] = $mysqli->query($queryLecturePeriode4)-> fetch_row()[0];
+    
+        $queryLecturePeriode5 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Afficher le contenu d\'un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-04-01' AND '2009-04-15'";
+        $LecturePeriode[4] = $mysqli->query($queryLecturePeriode5)-> fetch_row()[0];
+    
+        $queryLecturePeriode6 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Afficher le contenu d\'un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-04-15' AND '2009-04-31'";
+        $LecturePeriode[5] = $mysqli->query($queryLecturePeriode6)-> fetch_row()[0];
+    
+        $queryLecturePeriode7 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Afficher le contenu d\'un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-05-01' AND '2009-05-15'";
+        $LecturePeriode[6] = $mysqli->query($queryLecturePeriode7)-> fetch_row()[0];
+    
+        $queryLecturePeriode8 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Afficher le contenu d\'un message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-05-15' AND '2009-05-31'";
+        $LecturePeriode[7] = $mysqli->query($queryLecturePeriode8)-> fetch_row()[0];
+
+        //Contient le nombre de documents (posté ou téléchargé) par période de 15 jours
+        $DocumentPeriode = array();
+        $queryDocumentPeriode1 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Download un fichier dans le message' OR `Titre`= 'Upload un ficher avec le message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-02-01' AND '2009-02-15'";
+        $DocumentPeriode[0] = $mysqli->query($queryDocumentPeriode1)-> fetch_row()[0];
+    
+        $queryDocumentPeriode2 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Download un fichier dans le message' OR `Titre`= 'Upload un ficher avec le message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-02-15' AND '2009-02-29'";
+        $DocumentPeriode[1] = $mysqli->query($queryDocumentPeriode2)-> fetch_row()[0];
+    
+        $queryDocumentPeriode3 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Download un fichier dans le message' OR `Titre`= 'Upload un ficher avec le message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-03-01' AND '2009-03-15'";
+        $DocumentPeriode[2] = $mysqli->query($queryDocumentPeriode3)-> fetch_row()[0];
+    
+        $queryDocumentPeriode4 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Download un fichier dans le message' OR `Titre`= 'Upload un ficher avec le message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-03-15' AND '2009-03-31'";
+        $DocumentPeriode[3] = $mysqli->query($queryDocumentPeriode4)-> fetch_row()[0];
+    
+        $queryDocumentPeriode5 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Download un fichier dans le message' OR `Titre`= 'Upload un ficher avec le message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-04-01' AND '2009-04-15'";
+        $DocumentPeriode[4] = $mysqli->query($queryDocumentPeriode5)-> fetch_row()[0];
+    
+        $queryDocumentPeriode6 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Download un fichier dans le message' OR `Titre`= 'Upload un ficher avec le message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-04-15' AND '2009-04-31'";
+        $DocumentPeriode[5] = $mysqli->query($queryDocumentPeriode6)-> fetch_row()[0];
+    
+        $queryDocumentPeriode7 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Download un fichier dans le message' OR `Titre`= 'Upload un ficher avec le message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-05-01' AND '2009-05-15'";
+        $DocumentPeriode[6] = $mysqli->query($queryDocumentPeriode7)-> fetch_row()[0];
+    
+        $queryDocumentPeriode8 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Download un fichier dans le message' OR `Titre`= 'Upload un ficher avec le message') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-05-15' AND '2009-05-31'";
+        $DocumentPeriode[7] = $mysqli->query($queryDocumentPeriode8)-> fetch_row()[0];
+
+        //Contient le nombre de connexion (posté ou téléchargé) par période de 15 jours
+        $ConnexionPeriode = array();
+        $queryConnexionPeriode1 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Connexion') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-02-01' AND '2009-02-15'";
+        $ConnexionPeriode[0] = $mysqli->query($queryConnexionPeriode1)-> fetch_row()[0];
+    
+        $queryConnexionPeriode2 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Connexion') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-02-15' AND '2009-02-29'";
+        $ConnexionPeriode[1] = $mysqli->query($queryConnexionPeriode2)-> fetch_row()[0];
+    
+        $queryConnexionPeriode3 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Connexion') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-03-01' AND '2009-03-15'";
+        $ConnexionPeriode[2] = $mysqli->query($queryConnexionPeriode3)-> fetch_row()[0];
+    
+        $queryConnexionPeriode4 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Connexion') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-03-15' AND '2009-03-31'";
+        $ConnexionPeriode[3] = $mysqli->query($queryConnexionPeriode4)-> fetch_row()[0];
+    
+        $queryConnexionPeriode5 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Connexion') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-04-01' AND '2009-04-15'";
+        $ConnexionPeriode[4] = $mysqli->query($queryConnexionPeriode5)-> fetch_row()[0];
+    
+        $queryConnexionPeriode6 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Connexion') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-04-15' AND '2009-04-31'";
+        $ConnexionPeriode[5] = $mysqli->query($queryConnexionPeriode6)-> fetch_row()[0];
+    
+        $queryConnexionPeriode7 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Connexion') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-05-01' AND '2009-05-15'";
+        $ConnexionPeriode[6] = $mysqli->query($queryConnexionPeriode7)-> fetch_row()[0];
+    
+        $queryConnexionPeriode8 = "SELECT Count(IDTran) as nb FROM `transition` WHERE (`Titre`= 'Connexion') AND `Utilisateur` ='".$nom."' AND `Date` BETWEEN '2009-05-15' AND '2009-05-31'";
+        $ConnexionPeriode[7] = $mysqli->query($queryConnexionPeriode8)-> fetch_row()[0];
+
+        $MotivationPeriode = array();
+        for ($i = 0; $i <= 7; $i++) {
+            $MotivationPeriode[$i] = $MessagePeriode[$i] * 1 + $LecturePeriode[$i] * 0.5 + $DocumentPeriode[$i] * 1.3 + $ConnexionPeriode[$i] * 0.7;
+            $MotivationPeriode[$i] = round(($MotivationPeriode[$i]/1000) * 100,2);
+        }
+
+
+        $JSONResult[$nom]["MessagePeriode"] = $MessagePeriode;
+        $JSONResult[$nom]["LecturePeriode"] = $LecturePeriode;
+        $JSONResult[$nom]["DocumentPeriode"] = $DocumentPeriode;
+        $JSONResult[$nom]["ConnexionPeriode"] = $ConnexionPeriode;
+        $JSONResult[$nom]["MotivationPeriode"] = $MotivationPeriode;
+    } 
+    
+    //Création d'un fichier JSON avec les résultats
+    $fp = fopen('results.json', 'w');
+    fwrite($fp, json_encode($JSONResult));
+    fclose($fp);
+    mysqli_close($mysqli);   
+?>
--- relation.js
+++ relation.js
@@ -1,7 +1,250 @@
+const data = {
+    Utilisateurs: [
+      {
+        Login: 'Thierry09',
+        Activites: {
+          Connexions: [
+            {
+              Date: '01/25/52',
+              Heure: '12h32',
+              Duree: 120
+            },
+            {
+              Date: '01/25/52',
+              Heure: '12h32',
+              Duree: 212
+            }
+          ],
+          Messages_Lu: [
+            {
+              ID_Message: '01/25/52',
+              Date: '12h32',
+              Heure: 56
+            },
+            {
+              ID_Message: '01/25/52',
+              Date: '12h32',
+              Heure: 56
+            }
+          ],
+          Messages_Postes: [
+            {
+              ID_Message: 4,
+              Id_Forum: 2,
+              Date: '12h32',
+              Heure: '125'
+            },
+            {
+              ID_Message: 4,
+              Id_Forum: 2,
+              Date: '01/25/52',
+              Heure: '12h32'
+            }
+          ],
+          Visites_Hyperlien: [
+            {
+              ID_Message: 12,
+              Date: '01/25/52',
+              Heure: '12h32'
+            },
+            {
+              ID_Message: 12,
+              Date: '01/25/52',
+              Heure: '12h32'
+            }
+          ],
+          Reponses_Message: [
+            {
+              ID_Parent: 1,
+              ID_Message: 12,
+              Date: '01/25/52',
+              Heure: '12h32'
+            },
+            {
+              ID_Parent: 1,
+              ID_Message: 12,
+              Date: '01/25/52',
+              Heure: '12h32'
+            }
+          ],
+          Documents_Deposes: [
+            {
+              ID_Fichier: 1,
+              Date: '12/12/2512',
+              Heure: '12h32',
+              ID_Message: 37,
+              ID_Forum: 58,
+              Nb_Telechargements: 3
+            },
+            {
+              ID_Fichier: 1,
+              Date: '12/12/2512',
+              Heure: '12h32',
+              ID_Message: 37,
+              ID_Forum: 58,
+              Nb_Telechargements: 3
+            }
+          ],
+          Documents_Telecharges: [
+            {
+              ID_Fichier: 1,
+              ID_Forum: 3,
+              ID_Message: 4,
+              Date: '12/12/2021',
+              Heure: '12h31'
+            },
+            {
+              ID_Fichier: 1,
+              ID_Forum: 3,
+              ID_Message: 4,
+              Date: '12/12/2021',
+              Heure: '12h31'
+            }
+          ]
+        }
+      },
+      {
+        Login: 'Thierry0wxw9',
+        Activites: {
+          Connexions: [
+            {
+              Date: '01/25/52',
+              Heure: '12h32',
+              Duree: 120
+            },
+            {
+              Date: '01/25/52',
+              Heure: '12h32',
+              Duree: 212
+            }
+          ],
+          Messages_Lu: [
+            {
+              ID_Message: '01/25/52',
+              Date: '12h32',
+              Heure: 56
+            },
+            {
+              ID_Message: '01/25/52',
+              Date: '12h32',
+              Heure: 56
+            }
+          ],
+          Messages_Postes: [
+            {
+              ID_Message: 4,
+              Id_Forum: 2,
+              Date: '12h32',
+              Heure: '125'
+            },
+            {
+              ID_Message: 4,
+              Id_Forum: 2,
+              Date: '01/25/52',
+              Heure: '12h32'
+            }
+          ],
+          Visites_Hyperlien: [
+            {
+              ID_Message: 12,
+              Date: '01/25/52',
+              Heure: '12h32'
+            },
+            {
+              ID_Message: 12,
+              Date: '01/25/52',
+              Heure: '12h32'
+            }
+          ],
+          Reponses_Message: [
+            {
+              ID_Parent: 1,
+              ID_Message: 12,
+              Date: '01/25/52',
+              Heure: '12h32'
+            },
+            {
+              ID_Parent: 1,
+              ID_Message: 12,
+              Date: '01/25/52',
+              Heure: '12h32'
+            }
+          ],
+          Documents_Deposes: [
+            {
+              ID_Fichier: 1,
+              Date: '12/12/2512',
+              Heure: '12h32',
+              ID_Message: 37,
+              ID_Forum: 58,
+              Nb_Telechargements: 3
+            },
+            {
+              ID_Fichier: 1,
+              Date: '12/12/2512',
+              Heure: '12h32',
+              ID_Message: 37,
+              ID_Forum: 58,
+              Nb_Telechargements: 3
+            }
+          ],
+          Documents_Telecharges: [
+            {
+              ID_Fichier: 1,
+              ID_Forum: 3,
+              ID_Message: 4,
+              Date: '12/12/2021',
+              Heure: '12h31'
+            },
+            {
+              ID_Fichier: 1,
+              ID_Forum: 3,
+              ID_Message: 4,
+              Date: '12/12/2021',
+              Heure: '12h31'
+            }
+          ]
+        }
+      }
+    ]
+  }
+
+
+  function moulinette_relation(){
+      let i = 0;
+      let sortie = [];
+      //Pour chaque utilisateur
+      data.Utilisateurs.forEach(utilisateur =>{
+          let nb_Connexion = utilisateur.Activites.Connexions.length;
+          let temps_Connexion = utilisateur.Activites.Connexions[0].Duree;
+          console.log(temps_Connexion);
+          let nbMessagelu = 0;
+          utilisateur.Activites.Messages_Lu.forEach(messageLu => {
+            nbMessagelu++;
+          });
+          console.log(nbMessagelu);
+          sortie[i] = {
+              name : utilisateur.Login,
+              data : [
+                  1,1,1
+              ]
+          }
+          i++;
+      });
+      console.log(sortie);
+  }
+
+
+
+
+
+
+
+
 Highcharts.chart('container', {
 
   title: {
-    text: 'Highcharts Dependency Wheel'
+    text: 'Roue des relations'
   },
 
   accessibility: {
@@ -13,56 +256,21 @@ Highcharts.chart('container', {
   series: [{
     keys: ['from', 'to', 'weight'],
     data: [
-      ['Brazil', 'Portugal', 5],
-      ['Brazil', 'France', 1],
-      ['Brazil', 'Spain', 1],
-      ['Brazil', 'England', 1],
-      ['Canada', 'Portugal', 1],
-      ['Canada', 'France', 5],
-      ['Canada', 'England', 1],
-      ['Mexico', 'Portugal', 1],
-      ['Mexico', 'France', 1],
-      ['Mexico', 'Spain', 5],
-      ['Mexico', 'England', 1],
-      ['USA', 'Portugal', 1],
-      ['USA', 'France', 1],
-      ['USA', 'Spain', 1],
-      ['USA', 'England', 5],
-      ['Portugal', 'Angola', 2],
-      ['Portugal', 'Senegal', 1],
-      ['Portugal', 'Morocco', 1],
-      ['Portugal', 'South Africa', 3],
-      ['France', 'Angola', 1],
-      ['France', 'Senegal', 3],
-      ['France', 'Mali', 3],
-      ['France', 'Morocco', 3],
-      ['France', 'South Africa', 1],
-      ['Spain', 'Senegal', 1],
-      ['Spain', 'Morocco', 3],
-      ['Spain', 'South Africa', 1],
-      ['England', 'Angola', 1],
-      ['England', 'Senegal', 1],
-      ['England', 'Morocco', 2],
-      ['England', 'South Africa', 7],
-      ['South Africa', 'China', 5],
-      ['South Africa', 'India', 1],
-      ['South Africa', 'Japan', 3],
-      ['Angola', 'China', 5],
-      ['Angola', 'India', 1],
-      ['Angola', 'Japan', 3],
-      ['Senegal', 'China', 5],
-      ['Senegal', 'India', 1],
-      ['Senegal', 'Japan', 3],
-      ['Mali', 'China', 5],
-      ['Mali', 'India', 1],
-      ['Mali', 'Japan', 3],
-      ['Morocco', 'China', 5],
-      ['Morocco', 'India', 1],
-      ['Morocco', 'Japan', 3],
-      ['Japan', 'Brazil', 1]
+      ['Juliane', 'Manuel', 1],
+      ['Juliane', 'Maxime', 2],
+      ['Juliane', 'Tom', 3],
+      ['Tom', 'Manuel', 3],
+      ['Tom', 'Juliane', 2],
+      ['Tom', 'Maxime', 1],
+      ['Maxime', 'Manuel', 2],
+      ['Maxime', 'Juliane', 1],
+      ['Maxime', 'Tom', 3],
+      ['Manuel', 'Maxime', 5],
+      ['Manuel', 'Juliane', 2],
+      ['Manuel', 'Tom', 1]
     ],
     type: 'dependencywheel',
-    name: 'Dependency wheel series',
+    name: 'Poids de la relation',
     dataLabels: {
       color: '#333',
       textPath: {
@@ -77,3 +285,35 @@ Highcharts.chart('container', {
   }]
 
 });
+
+$('#btn').bind('click', function() {
+  var chart1 = $('#container').highcharts();
+  //var juTom = document.getElementById('JuTom');
+  //var juMax = document.getElementById('JuMax');
+  var juManu = document.getElementById('JuManu');
+
+  var test = Number(juManu.value);
+  console.log("VALEUR "+test);
+
+  chart1.series[0].update({
+    data: [
+      ['Juliane', 'Manuel', test],
+      ['Juliane', 'Maxime', 8],
+      ['Juliane', 'Tom', 9],
+      ['Tom', 'Manuel', 5],
+      ['Tom', 'Juliane', 5],
+      ['Tom', 'Maxime', 5],
+      ['Maxime', 'Manuel', 5],
+      ['Maxime', 'Juliane', 5],
+      ['Maxime', 'Tom', 5],
+      ['Manuel', 'Maxime', 5],
+      ['Manuel', 'Juliane', 5],
+      ['Manuel', 'Tom', 5]
+    ]
+  }, false);
+
+  chart1.redraw();
+
+});
+
+moulinette_relation();
